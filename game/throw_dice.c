35175edef284ee4a439938d66a05746c2e990022
Genki Hashimoto <knct.gen@gmail.com>
2016-01-08 13:16:19 +0900

    ヘッダ作成

--- game/throw_dice.c
+++ game/throw_dice.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "throw_dice.h"
 
 int throw_dice(int n)
 {
---
+++ game/throw_dice.h
@@ -0,0 +1,6 @@
+#ifndef _THROW_DICE_H_
+#define _THROW_DICE_H_
+
+throw_dice(int n);
+
+#endif // _THROW_DICE_H_
