7e1c4f0f813121454ff313f0a4843529ccb41062
Genki Hashimoto <knct.gen@gmail.com>
2016-01-08 14:32:17 +0900

    program_init作業途中

---
+++ game/program_init.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "def.h"
+#include "var.h"
+
+TROUT_T base_callender[9][CALENDER_LEN];
+
+int load_base_callender(void)
+{
+	TROUT_T day;
+	
+}
+int program_init(void)
+{
+	srand(unsigned(time)NULL)
+
+}
+
+int main(void) {
+}
---
+++ game/program_init.h
@@ -0,0 +1,4 @@
+#ifndef _PROGRAM_INIT_H_
+#define _PROGRAM_INIT_H_
+
+#endif // _PROGRAM_INIT_H_
