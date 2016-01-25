390687294874ed42c74eb9ba57a5e74ef26179e3
takunoko <takunoko@gmail.com>
2016-01-25 14:09:48 +0900

    プレイヤーステータス変更まで完成。move_playerの終了処理は書いていない

--- test/test-move_player.c
+++ test/test-move_player.c
@@ -14,10 +14,18 @@ int main(void){
 
     program_init(base_cal);
 
+    // ユーザ入力によるプレイヤー情報の初期化
     strncpy(player.name, "aaa", 10);
     player.grade = 3;
     player.club = CLUB_SPORT;
     player.girlfriend = 1;
+
+    // 定数としてのプレイヤー情報初期化
+    player.enhance_p   = 100;  // 充実ポイント
+    player.task_p      = 3000; // 課題ポイント
+    player.girlfriend  = 0;    // 彼女の有無 (確率でやりたい)
+    player.money_p     = 1000; // 金
+    player.day         = 0;    // 現在の日付
 
     init_calender(base_cal, player.grade, player.club, player.girlfriend, player.calender);
     // とりあえず各種カレンダーを初期化して表示まで
@@ -27,15 +35,16 @@ int main(void){
     // 10回ぐらい適当に行動させて、その時々のステータスを表示する
     int old_day;
     int i;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 3; i++) {
         old_day = player.day;
         move_player(&player);
-        printf("%10s: %d\n", "出目", old_day-player.day);
+        printf("----------------------------------------\n");
+        printf("%20s: %d\n", "出目", player.day-old_day);
         puts("");
-        printf("%10s: %d\n", "日付", player.day);
-        printf("%10s: %d\n", "充実ポイント", player.day);
-        printf("%10s: %d\n", "課題ポイント", player.day);
-        printf("%10s: %d\n", "金", player.day);
+        printf("%20s: %d\n", "日付", player.day);
+        printf("%20s: %d\n", "充実ポイント", player.enhance_p);
+        printf("%20s: %d\n", "課題ポイント", player.task_p);
+        printf("%20s: %d\n", "金", player.money_p);
     }
 
     return 0;
