79c1ebc046acf64b6226fe01235118464b0070c2
takunoko <takunoko@gmail.com>
2016-02-15 11:56:59 +0900

    一応ゲーム終了の描画は最低限完成

--- game/game_end.c
+++ game/game_end.c
@@ -21,6 +21,9 @@ void game_end(WINDOW *win, PLAYER_T players[]){
     int id[]={0,1,2,3};
     int use_id[4] = {0};
 
+    int x_pos = 100/2 - (40/2);
+    int yy;
+
     // とりあえず，充実ポイント順にソート
     for (i = 0; i < 4; i++) {
         for (j = i; j < 4; j++) {
@@ -29,20 +32,29 @@ void game_end(WINDOW *win, PLAYER_T players[]){
         }
     }
 
+    // タイトルの表示
+    yy = 5;
+    mvwprintw(win, yy, x_pos+15,"XXXの夏休み");
+    mvwprintw(win, yy+2, x_pos+50," 製作: 2班");
+
+    // 課題ポイントが残っていない人
     for (i = 0; i < 4; i++) {
         if (players[id[i]].task_p <= 0){
-            mvwprintw(win,r_cnt+3, 4, "%d位: %s, 充実P: %6d    課題P: %6d", r_cnt+1, players[id[i]].name, players[id[i]].enhance_p, players[id[i]].task_p);
+            mvwprintw(win,r_cnt+20, x_pos, "%d位: %s, 充実P: %6d 課題P: %6d", r_cnt+1, players[id[i]].name, players[id[i]].enhance_p, players[id[i]].task_p);
             r_cnt++;
             use_id[i] = 1;
         }
     }
 
+    // 課題ポイントが残っている人
     for (i = 0; i < 4; i++) {
         if (use_id[i] == 0){
-            mvwprintw(win,r_cnt+3, 4, "%d位: %s, 充実P: %6d    課題P: %6d", r_cnt+1, players[id[i]].name, players[id[i]].enhance_p, players[id[i]].task_p);
+            mvwprintw(win,r_cnt+20, x_pos, "%d位: %s, 充実P: %6d 課題P: %6d", r_cnt+1, players[id[i]].name, players[id[i]].enhance_p, players[id[i]].task_p);
             r_cnt++;
         }
     }
+
+    mvwprintw(win, 30, x_pos+4, "qキーを押してゲームを終了します。");
 
     wwait_q(win);
 }
