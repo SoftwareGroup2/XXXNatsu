d7c5cc9a9e8a1454824d4cef35fafcf304a66a29
takunoko <takunoko@gmail.com>
2016-02-15 15:27:06 +0900

    テストのdisp_gameが完成

--- game/disp_game_window.h
+++ game/disp_game_window.h
@@ -2,5 +2,6 @@
 #define _DISP_GAME_WINDOW_H_
 void create_cal(WINDOW *win, int y, int x,PLAYER_T p[]);
 void create_info(WINDOW *win,int y,int x,PLAYER_T p[]);
-void create_ive(); // 未定
+void create_ive(WINDOW *win,int y,int x,PLAYER_T p);
+void main_2(WINDOW *win, PLAYER_T players[4], int player_id);
 #endif // _DISP_GAME_WINDOW_H_
--- test/test-disp_game_window.c
+++ test/test-disp_game_window.c
@@ -1,4 +1,5 @@
 #include <curses.h>
+#include <locale.h>
 
 #include "use_curses.h"
 #include "def.h"
@@ -22,57 +23,39 @@ void set(PLAYER_T p[])
     }
 }
 
-int main(void){
-    WINDOW *win,*cal,*ive,*info;
+int main(){
+    WINDOW *win;
+    PLAYER_T players[4];
+    int player_id = 0;
     char c;
-    int i;
-    PLAYER_T p[4];
-    char str[2000];
 
-    // cursesの初期化
-    init_curses();
+    set(players);
 
-    // プレイヤーステータスの初期化
-    set(p);
+    setlocale(LC_ALL,"");
 
-    // プレイヤーを適当な位置に移動
-    p[0].day = 0;
-    p[1].day = 10;
-    p[2].day = 25;
-    p[3].day = 40;
+    /* 初期化 */
+    initscr();
+
+    /* エコー／バッファリング禁止 */
+    noecho();
+    cbreak();
+
 
     /* 全体ウィンドウ作成 */
-    i=0;
-
-    win = newwin(35,99,0,0);
+    win = newwin(35,100,0,0);
     box(win,'|','-');
 
-    // この部分をmain以外のどこかに移行が必要
-    sprintf(str,"%sの夏休み",p[i].name);
-    mvwaddstr(win,1,(COLS-1)/2,str);
+    /* サブウィンドウ作成 */
+    main_2(win,players,player_id);
 
-    /* カレンダーウィンドウ作成 */
-    cal = subwin(win,25,50,2,0);
-    create_cal(cal,1,1,p);
-    wrefresh(cal);
-
-    /* イベントウィンドウ作成 */
-    ive = subwin(win,25,49,2,50);
-    create_ive(ive);
-
-    /* プレイヤー情報ウィンドウ作成 */
-    info = subwin(win,8,99,27,0);
-    create_info(info,1,1,p);
-    wrefresh(info);
-
-    /* Escが押されるまで待機 */
+    /* メッセージ描画 */
     while((c = wgetch(win)) != 0x1b){
-        wrefresh(win);
     }
 
     /* 後処理 */
     wclear(win);
     wrefresh(win);
+    endwin();
 
-    end_curses();
+    return 0;
 }
