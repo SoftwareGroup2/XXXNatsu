0aadc2d0bcac914483e7f349f5b07a27f803f8b4
takunoko <takunoko@gmail.com>
2016-02-15 13:42:04 +0900

    swp

---
+++ game/main.c
@@ -0,0 +1,57 @@
+// ゲーム本体のプログラム
+
+#include <curses.h>
+#include <stdlib.h>
+
+#include "def.h"
+#include "var.h"
+
+#include "program_init.h"
+#include "init_player.h"
+#include "init_players.h"
+#include "init_calender.h"
+#include "throw_dice.h"
+#include "use_curses.h"
+#include "select_active_player.h"
+
+void game_main(WINDOW *main_win, TROUT_T base_cal[CALENDER_KIND][CALENDER_LEN+1], PLAYER_T players[4]){
+    int i,j;
+
+    int move_player_id;
+
+    // 最後のプレイヤーがゴールするまでループ
+    while(players[move_player_id = select_active_player(players)].day <= 44){
+        move_plaa
+
+}
+
+int main(int argc, const char *argv[])
+{
+    WINDOW *main_win;
+    TROUT_T base_cal[CALENDER_KIND][CALENDER_LEN+1];
+    PLAYER_T players[4];
+
+    // プログラムの初期化(カレンダーの初期化)
+    if(program_init(base_cal) == FALSE){
+        // 異常終了でプログラム終了
+        exit(-1);
+    }
+
+    // cursesの初期化
+    init_curses();
+    // 画面サイズのメインウインドウを生成
+    main_win = newwin(35,100,0,0);
+    box(main_win,'|','-');
+
+
+    // プレイヤーの初期化
+    init_players(main_win, players);
+
+    // ゲームメイン
+    game_main( main_win, base_cal, players);
+
+    // cursesの終了
+    end_curses();
+
+    return 0;
+}
